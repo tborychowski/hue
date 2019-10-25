33818c2f687004fc96d2c0702c90a1ce452b22c8
Tomasz Borychowski <tomasz.borychowski@sap.com>
2019-10-25 14:25:41 +0100

    chighhcharts > chartjs

--- src/index.js
+++ src/index.js
@@ -1,97 +1,42 @@
-const colors = ['#59a859', '#5797d0', '#ae5252', '#acac49', '#b9629b', '#34a892'];
-const config = {
-	type: 'line',
-	data: {labels: [], datasets: [] },
-	options: {
-		legend: { labels: { fontSize: 14, padding: 40, boxWidth: 16, fontColor: '#eee' } },
-		scales: {
-			xAxes: [{
-				type: 'time',
-				gridLines: { color: '#444' },
-				ticks: { fontColor: '#eee' },
-				time: {
-					displayFormats: {
-						minute: 'HH:mm',
-						hour: 'ddd HH:mm',
-						day: 'DD MMM, HH:mm'
-					}
-				}
-			}],
-			yAxes: [{
-				gridLines: { color: '#444' },
-				ticks: { fontColor: '#eee', callback: value => Math.round(value) + '°C' }
-			}]
-		},
-		tooltips: {
-			intersect: false,
-			mode: 'index',
-
-			titleMarginBottom: 12,
-			bodySpacing: 10,
-			footerSpacing: 30,
-
-			titleFontSize: 15,
-			bodyFontSize: 15,
-			footerFontSize: 15,
-			caretSize: 10,
-			callbacks: {
-				title: tt => {
-					const d = new Date(tt[0].xLabel);
-					const dat = d.toISOString().substr(0, 10);
-					const time = d.toLocaleTimeString().substr(0, 5);
-					return `${dat}  ${time}`;
-				},
-				label: (tt, data) => {
-					const label = data.datasets[tt.datasetIndex].label || '';
-					let val = Math.round(tt.yLabel * 100) / 100 + '°C';
-					return ` ${label} ${val}`;
-				},
-			}
-		}
-	},
-};
-let chart;
-const LIMIT = 168;    // should be 7 days, with 1h stams
-
-function fetchData () {
-	return fetch('src/data.csv').then(res => res.text());
-}
-
-
 function parseData (text) {
-	const json = {labels: [], datasets: []};
-	const lines = text.split('\n').filter(l => !!l);
-
+	const lines = text.trim().split('\n');
 	const header = lines.shift();
-
-	json.datasets = header.split(',')
+	const series = header.split(',')
 		.filter(l => l !== 'Date')
 		.map(l => { l = l.replace(' sensor', '').replace(' temp', ''); return l; })
-		.map((label, i) => ({
-			label,
-			borderColor: colors[i],
-			backgroundColor: colors[i],
-			borderWidth: 1,
-			fill: false,
-			data: []
-		}));
-
-	lines.slice(-LIMIT).forEach((line, l) => {
+		.map(name => ({ name, data: [] }));
+	lines.forEach(line => {
 		const [date, ...data] = line.split(',');
-		json.labels[l] = new Date(date);
-		json.datasets.forEach((set, s) => {
-			set.data.push(data[s]);
+		const d = +new Date(date);
+		series.forEach((s, idx) => {
+			s.data.push([ d, parseFloat(data[idx]) ]);
 		});
 	});
-	return json;
+	return series;
 }
 
-function drawChart (data) {
-	if (!chart) chart = new window.Chart(document.getElementById('chart'), config);
-	config.data = data;
-	chart.update();
+function formatter () {
+	return this.value + '°C';
 }
 
-fetchData()
+function drawChart (series) {
+	const config = {
+		scrollbar: { enabled: false },
+		credits: { enabled: false },
+		rangeSelector: { enabled: false },
+		chart: { style: { fontFamily: 'sans-serif' } },
+		legend: { enabled: true, align: 'center', verticalAlign: 'top', floating: false },
+		tooltip: { valueSuffix: '°C' },
+		yAxis: [
+			{ labels: { formatter }, opposite: false, showLastLabel: true },
+			{ labels: { formatter }, linkedTo: 0, showLastLabel: true }
+		],
+		series,
+	};
+	window.Highcharts.stockChart('chart', config);
+}
+
+fetch('src/data.csv')
+	.then(res => res.text())
 	.then(parseData)
 	.then(drawChart);
